8ebd35aab083ce24821d853827076582dfdbca56
Gabriel Roche <gabirocher@icloud.com>
2024-03-25 15:17:32 +0100

    Création page index.php + bouton déconnection

--- src/index.php
+++ src/index.php
@@ -7,6 +7,12 @@ if (!isset($_SESSION['username']) && !isset($_SESSION['password'])) {
 	exit();
 }
 
+if (isset($_POST['deconnect'])) {
+	session_destroy();
+	header("Location: ./login.php");
+	exit();
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -18,6 +24,26 @@ if (!isset($_SESSION['username']) && !isset($_SESSION['password'])) {
 		<script src="./script/jquery-3.7.0.min.js"></script>
 	</head>
 	<body>
+		<div id="body-container">
+			<nav id="index-nav">
+				<div id="logo-container">
+					<img id="logo" src="./images/ulhn.png">
+				</div>
+				<div class="white-space"></div>
+				<div class="white-space"></div>
+				<button class="nav-button">Vue d'ensemble</button>
+				<div class="half-white-space"></div>
+				<button class="nav-button">Visualisation</button>
+				<div class="white-space"></div>
+				<button class="nav-button">Importer</button>
+				<div class="half-white-space"></div>
+				<button class="nav-button">Exporter</button>
+				<div class="white-space"></div>
+				<form method="POST" class="ghost-element">
+					<button class="nav-button" name="deconnect">Déconnection</button>
+				</form>
+			</nav>
+		</div>
 		<script src="./script/all.js"></script>
 	</body>
 </html>
