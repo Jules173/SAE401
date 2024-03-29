9d77e893f5b92febce50649b71ee83bdafc0b365
Jules Chuzeville <115617916+Jules173@users.noreply.github.com>
2024-03-29 13:16:20 +0100

    Add files via upload

--- sql/script_creation.sql
+++ sql/script_creation.sql
@@ -1,51 +1,63 @@
+DROP TABLE IF EXISTS Etudiant CASCADE;
+DROP TABLE IF EXISTS Bin CASCADE;
+DROP TABLE IF EXISTS Semestre CASCADE;
+DROP TABLE IF EXISTS Users CASCADE;
+DROP TABLE IF EXISTS Administation CASCADE;
+DROP TABLE IF EXISTS Competence CASCADE;
+DROP TABLE IF EXISTS moyenne_competence CASCADE;
+DROP TABLE IF EXISTS validation CASCADE;
+DROP TABLE IF EXISTS Attribution CASCADE;
+DROP TABLE IF EXISTS moyenne_eleve CASCADE;
+DROP TABLE IF EXISTS Promotion CASCADE;
+
 CREATE TABLE Etudiant(
    etu_id INT,
-   codenip INT NOT NULL,
-   civ VARCHAR(4) NOT NULL,
-   nom VARCHAR(50) NOT NULL,
-   prenom VARCHAR(50) NOT NULL,
-   grpTD VARCHAR(1) NOT NULL,
-   grpTP VARCHAR(2) NOT NULL,
-   bac VARCHAR(50) NOT NULL,
+   codenip INT,
+   civ VARCHAR(4),
+   nom VARCHAR(50),
+   prenom VARCHAR(50),
+   grpTD VARCHAR(1),
+   grpTP VARCHAR(2),
+   bac VARCHAR(50),
    specialite VARCHAR(50),
    PRIMARY KEY(etu_id),
    UNIQUE(codenip)
 );
 
 CREATE TABLE Bin(
-   id_bin INT,
+   id_bin SERIAL,
    nom_bin VARCHAR(50),
    PRIMARY KEY(id_bin)
 );
 
 CREATE TABLE Semestre(
-   id_semestre INT,
-   semestre VARCHAR(50) NOT NULL,
+   id_semestre SERIAL,
+   semestre VARCHAR(50),
    PRIMARY KEY(id_semestre)
 );
 
 CREATE TABLE Users(
-   id_compte INT,
-   username VARCHAR(50) NOT NULL,
-   mdp VARCHAR(50) NOT NULL,
-   admin LOGICAL NOT NULL DEFAULT FALSE,
+   id_compte SERIAL,
+   username VARCHAR(50),
+   mdp VARCHAR(50),
+   admin LOGICAL DEFAULT FALSE,
    PRIMARY KEY(id_compte)
 );
 
 CREATE TABLE Administration(
    id_administration VARCHAR(50),
-   absence INT NOT NULL DEFAULT 0,
-   nb_justif_absence INT NOT NULL DEFAULT 0,
-   annee VARCHAR(50) NOT NULL,
-   etu_id INT NOT NULL,
+   absence INT DEFAULT 0,
+   nb_justif_absence INT DEFAULT 0,
+   annee VARCHAR(50),
+   etu_id INT,
    PRIMARY KEY(id_administration),
    FOREIGN KEY(etu_id) REFERENCES Etudiant(etu_id)
 );
 
 CREATE TABLE Competence(
-   id_competence INT,
-   nom_comp VARCHAR(50) NOT NULL,
-   id_semestre INT NOT NULL,
+   id_competence SERIAL,
+   nom_comp VARCHAR(50),
+   id_semestre INT,
    PRIMARY KEY(id_competence),
    FOREIGN KEY(id_semestre) REFERENCES Semestre(id_semestre)
 );
@@ -53,7 +65,7 @@ CREATE TABLE Competence(
 CREATE TABLE moyenne_competence(
    etu_id INT,
    id_competence INT,
-   bonus DECIMAL(4,2) NOT NULL DEFAULT 0,
+   bonus DECIMAL(4,2) DEFAULT 0,
    decision VARCHAR(50) DEFAULT 'ADM',
    PRIMARY KEY(etu_id, id_competence),
    FOREIGN KEY(etu_id) REFERENCES Etudiant(etu_id),
@@ -85,7 +97,7 @@ CREATE TABLE Attribution(
 CREATE TABLE moyenne_eleve(
    etu_id INT,
    id_bin INT,
-   moyenne DECIMAL(4,2) NOT NULL,
+   moyenne DECIMAL(4,2),
    PRIMARY KEY(etu_id, id_bin),
    FOREIGN KEY(etu_id) REFERENCES Etudiant(etu_id),
    FOREIGN KEY(id_bin) REFERENCES Bin(id_bin)
@@ -94,8 +106,8 @@ CREATE TABLE moyenne_eleve(
 CREATE TABLE Promotion(
    etu_id INT,
    id_semestre INT,
-   nom_status VARCHAR(50) NOT NULL DEFAULT 'Initial',
-   annee VARCHAR(50) NOT NULL,
+   nom_status VARCHAR(50) DEFAULT 'Initial',
+   annee VARCHAR(50),
    PRIMARY KEY(etu_id, id_semestre),
    FOREIGN KEY(etu_id) REFERENCES Etudiant(etu_id),
    FOREIGN KEY(id_semestre) REFERENCES Semestre(id_semestre)
