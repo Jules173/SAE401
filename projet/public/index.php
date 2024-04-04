87fec7d6005a2ac22104bcd6365743f2d212331d
eleonore <boulocheeleonore28@gmail.com>
2024-04-04 21:11:43 +0200

    index.php

--- projet/public/index.php
+++ projet/public/index.php
@@ -16,3 +16,34 @@ spl_autoload_register(function ($class_name) {
 });
 
 
+
+// Inclure le fichier de définition des routes
+$routes = include_once '../src/routes.php';
+$routes = include_once '../src/Router.php';
+
+// Créer une instance du routeur
+$router = new App\Router();
+
+// Ajouter les routes définies dans le fichier routes.php
+foreach ($routes as $route => $handlers) {
+	foreach ($handlers as $handler) {
+		// $handler[0] correspond à la méthode HTTP, $handler[1] correspond au chemin, $handler[2] correspond au contrôleur et à l'action
+		$router->addRoute($handler[0], $route . $handler[1], $handler[2][0], $handler[2][1]);
+	}
+}
+
+// Récupérer la méthode HTTP et le chemin de la requête
+$method = $_SERVER['REQUEST_METHOD'];
+$path = $_SERVER['REQUEST_URI'];
+
+try {
+	// Gérer la requête en utilisant le routeur
+	$router->handle($method, $path);
+} catch (\Exception $e) {
+	// Afficher une erreur 404 en cas de route non trouvée
+	header("HTTP/1.0 404 Not Found");
+	echo $e->getMessage();
+}
+
+
+
--- projet/src/router/routes.php
+++ projet/src/router/routes.php
@@ -5,12 +5,12 @@ require_once('../Controleur/EtudiantControleur.php');
 
 
 
-// Code pour définir les routes
+// Code pour définir les routes de l'application
 return [
 
 	// Routes pour les étudiants
 	'/etudiants' => [
-		// ['GET', '', ['App\Controleur\EtudiantControleur', 'getAllEtudiants']],
+		['GET', '', ['App\Controleur\EtudiantControleur', 'getAllEtudiants']],
 		['GET', '/{id}', ['App\Controleur\EtudiantControleur', 'getInfosEtudByID']],
 		['GET', '/{nom}/{prenom}', ['App\Controleur\EtudiantControleur', 'getInfosEtudByNomPrenom']],
 		['GET', '/moyenne/{idBin}/{idEtu}', ['App\Controleur\EtudiantControleur', 'getMoyByBin']]
